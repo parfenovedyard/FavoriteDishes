f1de47c6af15d4014ea706e4662abcc74b53d300
parfenovedyard <parfenovedyard@gmail.com>
2021-09-15 22:33:26 +0300

    splash activity animation

--- app/src/main/java/com/example/favdish/activities/SplashActivity.kt
+++ app/src/main/java/com/example/favdish/activities/SplashActivity.kt
@@ -1,10 +1,16 @@
 package com.example.favdish.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.favdish.R
 import com.example.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -22,5 +28,25 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        val splashAnimation =AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+                //
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+              //
+            }
+
+        })
     }
 }
