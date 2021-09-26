9d98b34f5fff2975090ebab35bea93f3e3f8192d
parfenovedyard <parfenovedyard@gmail.com>
2021-09-27 02:56:06 +0300

    set background color by Palette

--- app/src/main/java/com/example/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/DishDetailsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.favdish.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.favdish.R
 import com.example.favdish.databinding.FragmentDishDetailsBinding
 import java.io.IOException
@@ -27,11 +35,38 @@ class DishDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
-        args?.let {
+        args.let {
             try {
                 Glide.with(requireActivity())
                     .load(it.dishDetails.image)
                     .centerCrop()
+                    .listener(object: RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e("ups", "error loading Image")
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            resource.let {
+                            Palette.from(resource!!.toBitmap()).generate() { palette ->
+                                val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                binding!!.rlDishDetailMain.setBackgroundColor(intColor)
+                                }
+                            }
+                            return false
+                        }
+                    })
                     .into(binding!!.ivDishImage)
             } catch (e: IOException) {
                 e.printStackTrace()
