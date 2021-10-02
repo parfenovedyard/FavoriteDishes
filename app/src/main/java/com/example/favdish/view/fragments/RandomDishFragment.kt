a9588eebb558778fee283efb96d9a66cd594c86c
parfenovedyard <parfenovedyard@gmail.com>
2021-10-03 01:07:28 +0300

    preparing randomDish

--- app/src/main/java/com/example/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/RandomDishFragment.kt
@@ -4,42 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.favdish.databinding.FragmentRandomDishBinding
-import com.example.favdish.viewmodel.NotificationsViewModel
+
 
 class RandomDishFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentRandomDishBinding? = null
+    private var binding: FragmentRandomDishBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+    ): View {
+        binding = FragmentRandomDishBinding.inflate(layoutInflater)
 
-        _binding = FragmentRandomDishBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return binding!!.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
+
 }
