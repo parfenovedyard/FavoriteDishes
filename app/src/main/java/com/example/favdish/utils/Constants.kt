f85331f78b0596b3496220e72c896bbff797e14e
parfenovedyard <parfenovedyard@gmail.com>
2021-10-03 02:47:43 +0300

    preparing API calls

--- app/src/main/java/com/example/favdish/utils/Constants.kt
+++ app/src/main/java/com/example/favdish/utils/Constants.kt
@@ -9,6 +9,11 @@ object Constants {
     const val EXTRA_DISH_DETAILS: String = "DishDetails"
     const val ALL_ITEMS: String = "All"
     const val FILTER_SELECTION: String = "FilterSelection"
+    const val API_ENDPOINT: String = "recipes/random"
+    const val API_KEY: String = "apiKey"
+    const val LIMIT_LICENSE: String = "limitLicense"
+    const val TAGS: String = "tags"
+    const val NUMBER: String = "number"
 
     fun dishTypes(): ArrayList<String> {
         val list = ArrayList<String>()
