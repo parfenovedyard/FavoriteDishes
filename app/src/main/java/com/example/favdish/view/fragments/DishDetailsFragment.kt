6a96145633834c7b1508e02e0b1784471a15d4af
parfenovedyard <parfenovedyard@gmail.com>
2021-10-04 21:54:58 +0300

    share feature

--- app/src/main/java/com/example/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/DishDetailsFragment.kt
@@ -1,12 +1,13 @@
 package com.example.favdish.view.fragments
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
@@ -21,6 +22,8 @@ import com.bumptech.glide.request.target.Target
 import com.example.favdish.R
 import com.example.favdish.application.FavDishApplication
 import com.example.favdish.databinding.FragmentDishDetailsBinding
+import com.example.favdish.model.entities.FavDish
+import com.example.favdish.utils.Constants
 import com.example.favdish.viewmodel.FavDishViewModel
 import com.example.favdish.viewmodel.FavDishViewModelFactory
 import java.io.IOException
@@ -31,6 +34,60 @@ class DishDetailsFragment : Fragment() {
     private var binding: FragmentDishDetailsBinding? = null
     private val mFavDishModelFactory: FavDishViewModel by viewModels {
         FavDishViewModelFactory(((requireActivity().application) as FavDishApplication).repository)
+    }
+    private var mFavDishDetails: FavDish? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_share, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_share_dish ->{
+                val type = "text/plain"
+                val subject = "Checkout the dish recipe"
+                var extraText = ""
+                val shareWith = "Share with"
+                mFavDishDetails?.let {
+                    var image = ""
+                    if (it.imageSource == Constants.DISH_IMAGE_SOURCE_ONLINE) {
+                        image = it.image
+                    }
+                    val cookingInstructions: String
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        cookingInstructions = Html.fromHtml(
+                            it.directionToCook,
+                            Html.FROM_HTML_MODE_COMPACT
+                        ).toString()
+                    }else{
+                        @Suppress("DEPRECATION")
+                        cookingInstructions = Html.fromHtml(it.directionToCook).toString()
+                    }
+
+                    extraText =
+                        "$image \n" +
+                                "\n Title:  ${it.title} \n\n Type: ${it.type} \n\n" +
+                                " Category: ${it.category}" +
+                                "\n\n Ingredients: \n ${it.ingredients} \n\n " +
+                                "Instructions To Cook: \n $cookingInstructions" +
+                                "\n\n Time required to cook the dish approx ${it.cookingTime} minutes."
+                }
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = type
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+                intent.putExtra(Intent.EXTRA_TEXT, extraText)
+                startActivity(Intent.createChooser(intent, shareWith))
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateView(
@@ -44,6 +101,7 @@ class DishDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
+        mFavDishDetails = args.dishDetails
         args.let {
             try {
                 Glide.with(requireActivity())
@@ -68,7 +126,7 @@ class DishDetailsFragment : Fragment() {
                             isFirstResource: Boolean
                         ): Boolean {
                             resource.let {
-                            Palette.from(resource!!.toBitmap()).generate() { palette ->
+                            Palette.from(resource!!.toBitmap()).generate { palette ->
                                 val intColor = palette?.vibrantSwatch?.rgb ?: 0
                                 binding!!.rlDishDetailMain.setBackgroundColor(intColor)
                                 }
@@ -81,11 +139,22 @@ class DishDetailsFragment : Fragment() {
                 e.printStackTrace()
             }
             binding!!.tvTitle.text = it.dishDetails.title
+            @Suppress("DEPRECATION")
             binding!!.tvType.text =
                 it.dishDetails.type.capitalize(Locale.ROOT)
             binding!!.tvCategory.text = it.dishDetails.category
             binding!!.tvIngredients.text = it.dishDetails.ingredients
-            binding!!.tvCookingDirection.text = it.dishDetails.directionToCook
+            //binding!!.tvCookingDirection.text = it.dishDetails.directionToCook
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                binding!!.tvCookingDirection.text = Html.fromHtml(
+                    it.dishDetails.directionToCook,
+                    Html.FROM_HTML_MODE_COMPACT
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                binding!!.tvCookingDirection.text = Html.fromHtml(it.dishDetails.directionToCook)
+            }
+
             binding!!.tvCookingTime.text =
                 resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
             if (args.dishDetails.favoriteDish) {
