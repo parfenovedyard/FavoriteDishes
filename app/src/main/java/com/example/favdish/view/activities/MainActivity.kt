4ca7601b1ef2606ff937df694377477c02cf5d72
parfenovedyard <parfenovedyard@gmail.com>
2021-10-04 16:16:16 +0300

    retrieve API response at the UI and save to favorite and DB

--- app/src/main/java/com/example/favdish/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/favdish/view/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.favdish.view.activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -41,10 +42,12 @@ class MainActivity : AppCompatActivity() {
     fun hideBottomNavigationView() {
         binding.navView.clearAnimation()
         binding.navView.animate().translationY(binding.navView.height.toFloat()).duration = 300
+        binding.navView.visibility - View.GONE
     }
 
     fun showBottomNavigationView() {
         binding.navView.clearAnimation()
         binding.navView.animate().translationY(0f).duration = 300
+        binding.navView.visibility - View.VISIBLE
     }
 }
